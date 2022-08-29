3beff32eb5f65cf361f7506b90f10b15d0db0918
Denis <98394026+DenPyDev@users.noreply.github.com>
2022-08-29 17:07:18 +0300

    Update Store4.sol

--- Store4.sol
+++ Store4.sol
@@ -1,221 +1,179 @@
-const { expect } = require("chai")
-const { ethers} = require("hardhat");
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16 ;
+
+contract cryptoBlob4 {
+
+    mapping(address => mapping(bytes32 => bool)) public OWNER_PET_MAP;
+    mapping(address => bytes32[]) public OWNER_PET_LIST;
+    address[] public OWNERS_LIST;
+
+    mapping(bytes32 => uint256[]) public PET_GENES_MAP;
+
+    struct Claim {
+        address sender;
+        uint256 value;
+        bytes32 petName;
+        bytes32 newPetName;
+        uint256 timestamp;}
+
+    mapping(address => Claim[]) public INHERIT_TICKETS;
+
+    uint256 public SEED;
+
+    constructor(){
+        SEED = 1 + block.timestamp % 100;
+    }
+
+    function updateSeed(uint256 n) public {
+    unchecked{SEED = SEED + n;}
+    }
+
+    function randInt(uint256 from, uint256 to, uint256 seed) public view returns (uint256) {
+        uint256 rnd = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, seed))) % 100;
+        return from + (rnd * (to - from) / 100);
+    }
+
+    function readStr(string calldata text) public pure returns (bytes32){
+        bytes calldata bText = bytes(text);
+        require(bText.length <= 32, "string must be <= 32b");
+        return bytes32(bText);
+    }
+
+    function generateGenes(uint8 lenght) public view returns (uint256[] memory){
+        uint256[] memory genes = new uint256[](lenght);
+        uint256 i = 0;
+        while (i < lenght) {
+            genes[i] = randInt(50, 150, i + 1 + SEED);
+            i++;
+        }
+        return genes;
+    }
+
+    function deviateGenes(uint256[] memory genes) public view returns (uint256[] memory){
+        uint256[] memory tempGenes = genes;
+        uint256 i = 0;
+        uint256 newVal;
+        while (i < tempGenes.length) {
+            newVal = uint256(tempGenes[i] + uint256(randInt(0, 40, i + 1 + SEED)));
+            if (newVal > 20) {newVal -= 20;}
+            if (newVal > 200) {newVal -= 20;}
+            tempGenes[i] = newVal;
+            i++;
+        }
+        return tempGenes;
+    }
+
+    function setOwnerPet(address wallet, bytes32 petName) public {
+        OWNER_PET_MAP[wallet][petName] = true;
+        OWNER_PET_LIST[wallet].push(petName);
+        if (OWNER_PET_LIST[wallet].length < 2)
+        {OWNERS_LIST.push(wallet);}
+    }
+
+    function PUBgetOwnerPetList(address wallet) public view returns (string[] memory) {
+        uint256 lenght = OWNER_PET_LIST[wallet].length;
+
+        string[] memory output = new string[](lenght);
+        for (uint256 i = 0; i < OWNER_PET_LIST[wallet].length; i++)
+        {
+            output[i] = (string(abi.encodePacked(OWNER_PET_LIST[wallet][i])));
+        }
+        return output;
+    }
+
+    function containsOwnerPet(address wallet, bytes32 petName) public view returns (bool){
+        return OWNER_PET_MAP[wallet][petName];
+    }
+
+    function setPetGenes(bytes32 petName, uint256[] memory genes) public {
+        PET_GENES_MAP[petName] = genes;
+    }
+
+    function getPetGenes(bytes32 petName) public view returns (uint256[] memory){
+        return PET_GENES_MAP[petName];
+    }
+
+    function inherit(address wallet_to, bytes32 petName, bytes32 newPetName) public {
+        require(getPetGenes(petName).length >= 1, "error, parent name not exist");
+        require(getPetGenes(newPetName).length == 0, "error, child name already exist");
+        require(containsOwnerPet(msg.sender, petName) == true, "error,you are not the parent owner");
+
+        uint256[] memory petGenes = getPetGenes(petName);
+        updateSeed(petGenes.length);
+
+        uint256[] memory childGenes = deviateGenes(petGenes);
+        setPetGenes(newPetName, childGenes);
+        setOwnerPet(wallet_to, newPetName);
+    }
+
+    function arise(bytes32 petName) public {
+        require(getPetGenes(petName).length == 0, "error, name already exist");
+        uint256[] memory genes = generateGenes(20);
+        updateSeed(genes.length);
+        setPetGenes(petName, genes);
+        setOwnerPet(msg.sender, petName);
+    }
+
+    function PUBcheckPetNameExists(string calldata sPetName) public view returns (bool) {
+        bytes32 petName = readStr(sPetName);
+        return getPetGenes(petName).length > 0;
+    }
+
+    function PUBcheckAmIPetOwner(string calldata sPetName) public view returns (bool) {
+        bytes32 petName = readStr(sPetName);
+        return containsOwnerPet(msg.sender, petName);
+    }
+
+    function PUBgetPetGenesStr(string calldata sPetName) public view returns (uint256[] memory){
+        return PET_GENES_MAP[readStr(sPetName)];
+    }
+
+    function PUBarise(string calldata sPetName) public {
+        bytes32 petName = readStr(sPetName);
+        arise(petName);
+    }
+
+    function PUBinherit(address wallet_to, string calldata sPetName, string calldata sNewPetName) public {
+        bytes32 petName = readStr(sPetName);
+        bytes32 newPetName = readStr(sNewPetName);
+        inherit(wallet_to, petName, newPetName);
+    }
+
+    function PUBInheritCreateTicket(address petOwner, string calldata sPetName, string calldata sNewPetName) public payable {
+        bytes32 petName = readStr(sPetName);
+        bytes32 newPetName = readStr(sNewPetName);
+        INHERIT_TICKETS[petOwner].push(Claim(msg.sender, msg.value, petName, newPetName, block.timestamp));
+    }
+
+    function PUBcheckMyTickets() public view returns (Claim[] memory) {
+        return INHERIT_TICKETS[msg.sender];
+    }
+
+    function PUBacceptMyTicket(uint256 ticket_index) public {
+        require(ticket_index < INHERIT_TICKETS[msg.sender].length, "index out of range");
+        Claim memory ticket = INHERIT_TICKETS[msg.sender][ticket_index];
+        address payable addr = payable(msg.sender);
+        if (ticket.sender != address(0))
+        {
+            inherit(ticket.sender, ticket.petName, ticket.newPetName);
+            delete INHERIT_TICKETS[msg.sender][ticket_index];
+            addr.transfer(ticket.value);
+        }
+    }
+
+    function PUBrejectMyTicket(uint256 ticket_index) public {
+        require(ticket_index < INHERIT_TICKETS[msg.sender].length, "index out of range");
+        Claim memory ticket = INHERIT_TICKETS[msg.sender][ticket_index];
+        address payable addr = payable(ticket.sender);
+        if (ticket.sender != address(0))
+        {
+            delete INHERIT_TICKETS[msg.sender][ticket_index];
+            addr.transfer(ticket.value);
+        }
+    }
+
+    function PUBgetOwners() public view returns (address[] memory){
+        return OWNERS_LIST;
+    }
+}
 
-
-describe("cryptoBlob4", function () {
-  let acc1 //global
-  let acc2 //global
-  let ct //global
-
-  beforeEach(async function() {
-   
-
-    [acc1, acc2] = await ethers.getSigners(); //get wallets
-    const Payments = await ethers.getContractFactory("cryptoBlob4", acc1); //compile contract from acc1
-    ct = await Payments.deploy() ;  //deploy contract (sent tx)
-    await ct.deployed(); //deploy contract (wait tx to compile)
-    //console.log("payments.address", ct.address);
-
-  })
-  
-
-
-
-  it("seed should be increased", async function() {
-    const val_seed = await ct.SEED();
-    await ct.updateSeed(5);
-    const val_new_seed = await ct.SEED();
-    expect(Number(val_seed)+5).to.eq(Number(val_new_seed));
-  })
-
-
-
-  it("rnd should be in range, and random", async function() {
-    const allEqual = arr => arr.every(val => val === arr[0]);
-    const rndArr = [];
-    for(var i = 0; i < 10; i++){
-      const val_rnd = await ct.randInt(5000,5050,i);
-      expect(val_rnd).to.be.gte(5000);
-      expect(val_rnd).to.be.lte(5050);
-      rndArr.push(val_rnd);
-   }
-   //console.log("rndArr", rndArr);
-   expect(allEqual(rndArr)).to.eq(false);
-  })
-
-
-
-  it("should be possible to validate the string", async function() {
-    await ct.readStr("qwertyuiqwertyuiqwertyuiqwertyui");
-    await expect(ct.readStr("qwertyuiqwertyuiqwertyuiqwertyui_")).to.be.revertedWith("string must be <= 32b");
-  })
-
-
-  it("seed should be increased", async function() {
-    const Genes = await ct.generateGenes(5);
-    expect(Genes.length).to.eq(5);
-  })
-
-
-
-  it("should be generate random array", async function() {
-    const genes = await ct.generateGenes(5);
-    const newGenes = await ct.deviateGenes(genes);
-    expect(genes.length).to.eq(5);
-    expect(newGenes.length).to.eq(5);
-    expect(genes).to.not.eq(newGenes);
-
-  })
-  
-
-  it("should be check is acc ownes a pet", async function() {
-    const petName =  await ct.readStr("qwerty");
-    await ct.setOwnerPet(acc1.address, petName);
-
-    const ex = await ct.containsOwnerPet(acc1.address, petName);
-    const not_ex = await ct.containsOwnerPet(acc2.address, petName);
-
-    expect(ex).to.eq(true);
-    expect(not_ex).to.eq(false);
-  })
-
-
-
-
-  it("should be possible to set genes to pet", async function() {
-    const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
-    const petName =  await ct.readStr("qwerty");
-    await ct.setPetGenes(petName, [1,2,3]);
-
-    const origGenes = [ethers.BigNumber.from(1),ethers.BigNumber.from(2),ethers.BigNumber.from(3)]
-    const genes = await ct.getPetGenes(petName);
-    expect(equals(origGenes, genes)).to.eq(true);
-  })
-
-
-  it("should be possible to check pet existance", async function() {
-    const petName =  await ct.readStr("qwerty");
-    await ct.setPetGenes(petName, [1,2,3]);
-
-    expect(await ct.PUBcheckPetNameExists("qwerty")).to.eq(true);
-    expect(await ct.PUBcheckPetNameExists("qqwwee")).to.eq(false);
-  })
-
-
-
-
-
-
-  it("should be possible to check pes ownership", async function() {
-    const petName =  await ct.readStr("qwerty");
-    await ct.connect(acc1).setOwnerPet(acc1.address, petName);
-    expect(await ct.connect(acc1).PUBcheckAmIPetOwner("qwerty")).to.eq(true)
-    expect(await ct.connect(acc2).PUBcheckAmIPetOwner("qwerty")).to.eq(false)
-  })
-
-
-  it("should possible to create new pet", async function() {
-
-    await ct.connect(acc1).PUBarise("qwerty");
-    expect(await ct.PUBcheckPetNameExists("qwerty")).to.eq(true);
-    expect(await ct.connect(acc1).PUBcheckAmIPetOwner("qwerty")).to.eq(true)
-
-    const genes = await ct.PUBgetPetGenesStr("qwerty");
-    expect(genes.length).to.gte(1);
-
-    expect(await ct.connect(acc2).PUBcheckAmIPetOwner("qwerty")).to.eq(false)
-  })
-
-
-
-
-  it("should be possible to inherit from your pet", async function() {
-
-    const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
-    await ct.connect(acc1).PUBarise("qwerty");
-    await ct.PUBinherit(acc2.address,"qwerty", "qqwwee")
-
-    const parent_genes = await ct.PUBgetPetGenesStr("qwerty");
-    const child_genes = await ct.PUBgetPetGenesStr("qqwwee");
-
-    expect(equals(parent_genes, child_genes)).to.eq(false);
-    expect(parent_genes.length).to.eq(child_genes.length);
-  })
-
-  it("should be impossible to inherit from not your pet", async function() {
-    const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
-    await ct.connect(acc1).PUBarise("qwerty");
-    await expect(ct.connect(acc2).PUBinherit(acc2.address,"qwerty", "qqwwee")).to.be.revertedWith("error,you are not the parent owner");
-  })
-
-  
-  it("should be impossible to sreate inherit and accept ticket and eth", async function() {
-   
-    await ct.connect(acc1).PUBarise("qwerty");
-    
-    let acc1_balance = await ethers.provider.getBalance(acc1.address);
-    let acc2_balance = await ethers.provider.getBalance(acc2.address);
-    let contr_balance = await ethers.provider.getBalance(ct.address)
-
-    const options = {value: ethers.utils.parseUnits("1")}
-    await ct.connect(acc2).PUBInheritCreateTicket(acc1.address, "qwerty",  "qwerty-child", options);
-
-    let acc1_balance2 = await ethers.provider.getBalance(acc1.address);
-    let  acc2_balance2 = await ethers.provider.getBalance(acc2.address);
-    let contr_balance2 = await ethers.provider.getBalance(ct.address)
-
-    let one_eth_left_acc2 = (10**18 <= (acc2_balance - acc2_balance2)<1.1*10**18)
-    expect(one_eth_left_acc2 == true);
-
-    let one_eth_arrived_to_ct = (10**18 == (contr_balance2 - contr_balance))
-    expect(one_eth_arrived_to_ct == true);
-    
-    await ct.connect(acc1).PUBacceptMyTicket(0)
-
-    let acc1_balance3 = await ethers.provider.getBalance(acc1.address);
-    let acc2_balance3 = await ethers.provider.getBalance(acc2.address);
-    let contr_balance3 = await ethers.provider.getBalance(ct.address)
-
-    expect(contr_balance3 == 0);  //eth left contract's account
-    let diff3 = (0.9*10**18 <= (acc1_balance3 - acc1_balance )<=10**18)
-    expect(diff3 == true); //eth arrived to owner's account
-  })
-
-
-
-  it("should be impossible to sreate inherit and reject ticket and eth", async function() {
-   
-    await ct.connect(acc1).PUBarise("qwerty");
-    
-
-    let acc1_balance = await ethers.provider.getBalance(acc1.address);
-    let acc2_balance = await ethers.provider.getBalance(acc2.address);
-    let contr_balance = await ethers.provider.getBalance(ct.address)
-
-    const options = {value: ethers.utils.parseUnits("1")}
-    await ct.connect(acc2).PUBInheritCreateTicket(acc1.address, "qwerty",  "qwerty-child", options);
-
-    //let acc1_balance2 = await ethers.provider.getBalance(acc1.address);
-    let  acc2_balance2 = await ethers.provider.getBalance(acc2.address);
-    let contr_balance2 = await ethers.provider.getBalance(ct.address)
-
-    let one_eth_left_acc2 = (10**18 <= (acc2_balance - acc2_balance2)<1.1*10**18)
-    expect(one_eth_left_acc2 == true);
-
-    let one_eth_arrived_to_ct = (10**18 == (contr_balance2 - contr_balance));
-    expect(one_eth_arrived_to_ct == true);
-    
-    await ct.connect(acc1).PUBrejectMyTicket(0)
-
-    //let acc1_balance3 = await ethers.provider.getBalance(acc1.address);
-    let acc2_balance3 = await ethers.provider.getBalance(acc2.address);
-    let contr_balance3 = await ethers.provider.getBalance(ct.address)
-
-    expect(contr_balance3 == 0);  //eth left contract's account
-    let diff3 = ((acc2_balance3 - acc2_balance )<=0.1*10**18)
-    expect(diff3 == true); //eth arrived to sender's account
-  })
-
-})
-
- 
